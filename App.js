d83791ec6ec7b8ecc39bcf7aa9b54385fdfce570
dexter <typelogic@pm.me>
2022-01-17 01:00:29 +0800

    Learn more stuff

--- App.js
+++ App.js
@@ -80,7 +80,7 @@ const App: () => Node = () => {
           <Section title="Debug">
             <DebugInstructions />
           </Section>
-          <Section title="Learn More">
+          <Section title="Learn More Stuff">
             Read the docs to discover what to do next:
           </Section>
           <LearnMoreLinks />
